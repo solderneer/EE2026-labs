958a39e5d9a6466bc23b9330facbfd17a5eeb67e
Sudharshan <sudhar393@gmail.com>
2018-02-20 22:08:56 -0800

    Simple ALU

---
+++ calculator/calculator.srcs/sources_1/new/P_calculator.v
@@ -0,0 +1,32 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 20.02.2018 21:56:18
+// Design Name: 
+// Module Name: P_calculator
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module P_calculator(
+    input [3:0] X,
+    input [3:0] Y,
+    input [3:0] Z,
+    output [7:0] P
+    );
+    
+    // Add custom code below
+    
+endmodule
